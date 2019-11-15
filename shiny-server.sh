d4455f571dd0d90af963347b639c5a9a0f905bc3
Stuart <stuartlthomas@gmail.com>
2019-11-15 23:43:51 +1100

    Set shiny server to run as PUID
    Changed to start shiny server as user PUID ENV variable in actual call to shiny-server, rather than starting as root and relying on a run_as command inside shiny-server.conf.

--- shiny-server.sh
+++ shiny-server.sh
@@ -20,6 +20,13 @@ if [ -z "${PUID}" ]; then
   export PUID=shiny
 fi
 
+chown -R /data $PUID
+chown -R /code $PUID
+chown -R /01_input $PUID
+chown -R /02_code $PUID
+chown -R /04_output $PUID
+chown -R /var/log/shiny-server $PUID
+
 if [ ! -z "${SHINYCODE_GITHUB_REPO}" ];
 then
     echo "Copying contentes of github repo $SHINYCODE_GITHUB_REPO to $CODE_DIR"
@@ -32,8 +39,7 @@ envsubst < /etc/shiny-server/shiny-server.conf.tmpl >  /etc/shiny-server/shiny-s
 if [ "$DISCOVER_PACKAGES" = "true" ];
 then
     # scan files in $CODE_DIR for required libraries and install missing packages
-    ## install R-packages
-    #Rscript -e "source('/etc/shiny-server/install_discovered_packages.R'); discover_and_install(preinstalled_packages_csv = '/etc/shiny-server/preinstalled_packages.csv',r_search_root = '$CODE_DIR'); "
+    ## install R-packages    
 	Rscript -e "source('/etc/shiny-server/install_discovered_packages.R'); discover_and_install(default_packages_csv = '/etc/shiny-server/default_install_packages.csv', discovery_directory_root = '$CODE_DIR', discovery = TRUE);"
 else
     echo "DISCOVER_PACKAGES != true, Using preinstalled packages only"
@@ -41,10 +47,12 @@ fi
 
 if [ "$APPLICATION_LOGS_TO_STDOUT" = "false" ];
 then
-    exec shiny-server 2>&1
+    #exec shiny-server 2>&1 using user $PUID
+    exec /bin/su -c "shiny-server 2>&1" -  $PUID
 else
-    # start shiny server in detached mode
-    exec shiny-server 2>&1 &
+    # start shiny server in detached mode using user $PUID
+    #exec shiny-server 2>&1 &
+    exec /bin/su -c "shiny-server 2>&1" -  $PUID &
 
     # push the "real" application logs to stdout with xtail
     exec xtail /var/log/shiny-server/
